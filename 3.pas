015529f2c2515298090086392210077c60f83b4a
Laptya <144488450+Laptya@users.noreply.github.com>
2023-11-26 22:56:06 +0300

    Update 3.pas

--- 3.pas
+++ 3.pas
@@ -5,7 +5,7 @@ writeln('Введите текст');
 readln(text);
 writeln('Первый символ: ',text[1]);
 if length(text) mod 2 = 1 then
-writeln('Средний символ: ',text[length(text) div 2])
+writeln('Средний символ: ',text[length(text) div 2+1])
 else 
 writeln('Среднего символа нет');
 writeln('Последний символ: ',text[length(text)]);
